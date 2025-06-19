7e01f3124b33db31a27764da85bf3e409de0250a
HieuND <23010049@st.phenikaa-uni.edu.vn>
2025-06-19 23:36:21 +0700

    complete

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,7 +8,7 @@ class Room extends Model
 {
     // Nếu bạn dùng fillable, có thể thêm:
     // protected $fillable = ['name', 'city', 'price', 'image_path', 'describe'];
-    protected $fillable = ['name', 'city', 'price', 'image_path'];
+    protected $fillable = ['name', 'city', 'price', 'image_path','describe'];
     public function images()
     {
         return $this->hasMany(RoomImage::class);
